a3dcfbe706b47091b8ea5f97870a5d7058778614
fachinmax <maxfachin98@gmail.com>
2023-08-09 18:34:24 +0200

    api get city informations function now take an id

--- src/assets/scripts/api/api-get-city.js
+++ src/assets/scripts/api/api-get-city.js
@@ -1,4 +1,6 @@
-function apiGetCityInformations(endpoint) {
+function apiGetCityInformations(cityId) {
+    let rawHref = process.env.API_SINGLE_CITY
+    let endpoint = rawHref.replace(/code/, cityId)
     return fetch(endpoint).then(response => response.json())
 }
 
