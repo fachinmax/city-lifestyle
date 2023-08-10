491f066f9e38b57c1218892750c144827bde6a17
fachinmax <maxfachin98@gmail.com>
2023-08-10 13:23:57 +0200

    resolved some problems now won't throw errors when i add a new seach city container

--- src/assets/scripts/utils/handlers/add-search-city-container.js
+++ src/assets/scripts/utils/handlers/add-search-city-container.js
@@ -2,14 +2,14 @@ import { getForm } from '../../components/form'
 import { getInput } from '../../components/input'
 import { getUl } from '../../components/ul'
 import { getDiv } from '../../components/div'
+import { getButton } from '../../components/button'
 import { addHandlersToSearchContainer } from '../add-handlers-search-city-container'
 
 let count = 2
 
 function createForm(count) {
-    let form = getForm(`cities${count}`, `informations${count}`)
+    let form = getForm(`cities-${count}`, `informations-${count}`)
     let input = getInput('text', 'search-bar', 'search')
-    // addEvents(form, input)
     let ul = getUl('choises')
     form.append(input, ul)
     return form
@@ -19,9 +19,9 @@ function addSearchCityContainer(event) {
     if (count === 5) {
         this.hidden = true
     }
-    let searchCityContainer = getDiv(`search-city-container`)
+    let searchCityContainer = getDiv('search-city-container')
     let form = createForm(++count)
-    let informations = getDiv(`informations${count}`)
+    let informations = getDiv(`informations-${count}`)
     let remove = getButton('remove', '-')
     searchCityContainer.append(remove, form, informations)
     document
