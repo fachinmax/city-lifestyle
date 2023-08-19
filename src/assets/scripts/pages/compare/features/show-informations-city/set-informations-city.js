439203a681aa851222857f88fa82c1e3089b1fc3
fachinmax <maxfachin98@gmail.com>
2023-08-19 10:15:10 +0200

    update in order to use the new ver of write row

--- src/assets/scripts/pages/compare/features/show-informations-city/set-informations-city.js
+++ src/assets/scripts/pages/compare/features/show-informations-city/set-informations-city.js
@@ -4,11 +4,10 @@ import { writeNewRow } from './utils/write-new-row'
 
 function setInformationsCity(informations, table, indexCol) {
     let indexRows = 1
-    let moreRows = table.rows[1] ? false : true
 
     for (let [key, value] of Object.entries(informations)) {
         key = key[0].toUpperCase() + key.slice(1)
-        indexRows = writeNewRow(key, value, table, indexCol, indexRows, moreRows)
+        indexRows = writeNewRow(key, value, table, indexCol, indexRows)
     }
 }
 
--- src/assets/scripts/pages/compare/features/show-informations-city/set-informations-urban-area.js
+++ src/assets/scripts/pages/compare/features/show-informations-city/set-informations-urban-area.js
@@ -5,34 +5,33 @@ import { clearDescription } from '../../../../utils/clear-description'
 import { clearValue } from '../../../../utils/clear-value'
 import { writeNewRow } from './utils/write-new-row'
 
-function setDetails(info, table, indexCol, indexRow, addRows) {
+function setDetails(info, table, indexCol, indexRow) {
     let description, value
 
     info.data.forEach(data => {
         description = clearDescription(data.label)
         value = clearValue(data)
-        indexRow = writeNewRow(description, value, table, indexCol, indexRow, addRows)
+        indexRow = writeNewRow(description, value, table, indexCol, indexRow)
     })
 
     return indexRow
 }
 
-function setCategories(categories, details, table, indexCol, indexRow, addRows) {
+function setCategories(categories, details, table, indexCol, indexRow) {
     categories.forEach(categorie => {
         indexRow = writeNewRow(
             categorie.name,
             categorie.score_out_of_10.toFixed(2),
             table,
             indexCol,
-            indexRow,
-            addRows
+            indexRow
         )
 
         let relatedDetails = findRelatedInformations(details, categorie.name)
 
         if (!relatedDetails) return
 
-        indexRow = setDetails(relatedDetails, table, indexCol, indexRow, addRows)
+        indexRow = setDetails(relatedDetails, table, indexCol, indexRow)
     })
 
     return indexRow
@@ -41,15 +40,13 @@ function setCategories(categories, details, table, indexCol, indexRow, addRows) 
 function setInformationsUrbanArea(informations, table, indexCol) {
     console.dir(informations)
     let indexRows = 5
-    let moreRows = table.rows[5] ? false : true
 
     indexRows = setCategories(
         informations.dataScore.categories,
         informations.dataDetails,
         table,
         indexCol,
-        indexRows,
-        moreRows
+        indexRows
     )
     indexRows++
 }
