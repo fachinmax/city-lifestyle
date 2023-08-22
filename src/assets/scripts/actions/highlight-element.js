9220defc33270bbfa1a1d6a8212b187d76f4b8d4
fachinmax <maxfachin98@gmail.com>
2023-08-22 20:10:10 +0200

    text input update when li element is highlight

--- src/assets/scripts/actions/highlight-element.js
+++ src/assets/scripts/actions/highlight-element.js
@@ -1,28 +1,27 @@
 function highlightElement(event) {
+    let form = event.target.closest('form')
+    let input = form.elements['search-bar']
+    let target = event.target
+    let relatedTarget = relatedTarget
+
     switch (event.type) {
         case 'mouseover':
-            if (
-                event.target instanceof HTMLLIElement &&
-                !(event.relatedTarget instanceof HTMLLIElement)
-            ) {
-                event.target.setAttribute('data-highlight', true)
+            if (target instanceof HTMLLIElement && !(relatedTarget instanceof HTMLLIElement)) {
+                target.setAttribute('data-highlight', true)
+                input.value = target.textContent
             }
 
-            if (
-                event.target instanceof HTMLLIElement &&
-                event.relatedTarget instanceof HTMLLIElement
-            ) {
-                event.relatedTarget.setAttribute('data-highlight', false)
-                event.target.setAttribute('data-highlight', true)
+            if (target instanceof HTMLLIElement && relatedTarget instanceof HTMLLIElement) {
+                relatedTarget.setAttribute('data-highlight', false)
+                target.setAttribute('data-highlight', true)
+                input.value = input.textContent
             }
 
             break
         case 'mouseout':
-            if (
-                event.target instanceof HTMLLIElement &&
-                !(event.relatedTarget instanceof HTMLLIElement)
-            ) {
-                event.target.setAttribute('data-highlight', false)
+            if (target instanceof HTMLLIElement && !(relatedTarget instanceof HTMLLIElement)) {
+                target.setAttribute('data-highlight', false)
+                input.value = input.partialNameCity
             }
 
             break
--- src/assets/scripts/pages/compare/features/show-informations-city/index.js
+++ src/assets/scripts/pages/compare/features/show-informations-city/index.js
@@ -5,6 +5,7 @@ import { getRemoveInformationsCity } from '../remove-informations-city/index'
 import { showCities } from '../../../../actions/show-cities'
 import { scrollToChoises } from '../../../../actions/scroll-to-choises'
 import { showInformations } from './actions/show-informations'
+import { highlightElement } from '../../../../actions/highlight-element'
 
 function getShowInformationsCity() {
     let form = getForm()
@@ -12,10 +13,12 @@ function getShowInformationsCity() {
     showInformationsCity.classList.add('container-city-search')
     showInformationsCity.setAttribute('data-position', 'left')
     let searchBar = form.elements['search-bar']
+    let containerChoises = form.querySelector('ul')
     searchBar.oninput = showCities
     searchBar.onkeydown = showInformations
     searchBar.addEventListener('keydown', scrollToChoises)
     form.onsubmit = event => event.preventDefault()
+    containerChoises.onmouseover = containerChoises.onmouseout = highlightElement
     showInformationsCity.append(getRemoveInformationsCity(), form)
     return showInformationsCity
 }
